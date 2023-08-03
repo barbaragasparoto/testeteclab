3e95f4e2ca1dd2d5fd51948b65cffb50b42784d6
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-08-03 12:26:42 -0300

    teste inicial

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text >Teste</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#663399',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
